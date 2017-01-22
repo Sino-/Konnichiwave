5c7565366d12b44237f7e3b943108f1abbde7366
Isabel Withmory <amywithmory@MacBook-Pro.local>
2017-01-21 20:38:16 -0800

    leaderboard

---
+++ scripts/submitScore.php
@@ -0,0 +1,36 @@
+<?php
+
+  #### CONNECTING TO MYSQL ####
+  $HOST = 'localhost';
+  $USER = 'root';
+  $PASS = 'root';
+  $DBNAME = 'database_name';
+
+  $TABLE = 'highscore';
+
+  mysql_connect($HOST, $USER, $PASS) or die(mysql_error());
+  mysql_select_db($DBNAME);
+  ####  ####
+
+  // Bits of security: ensuring both username and score and sent into this request and score is numeric
+  if (empty($_POST['username']) || empty($_POST['score'] || !is_numeric($score))) {
+    echo "Both username and score parameters are needed";
+    exit();
+  }
+
+  // Pulling variables from post request
+  $username = htmlentities(mysql_real_escape_string($_POST['username']));
+  $score = htmlentities(mysql_real_escape_string($_POST['score']));
+
+  // Executing insert query into the database
+  $query = "INSERT INTO highscore(username, score) VALUES('$username', '$score')";
+  $result = mysql_query($query);
+
+  // Housekeeping
+  if ($result) {
+    echo "success";
+  } else {
+    echo "failure";
+  }
+
+?>
